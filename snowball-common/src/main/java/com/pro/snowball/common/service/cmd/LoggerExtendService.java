8d8140911931c415bdac54036a9409b954cdd97a
qaz4042 <404286846@qq.com>
2025-04-26 23:05:04 +0800

    fix: 修复 redis内存溢出问题 (socket推送不用经过redis)

--- snowball-common/src/main/java/com/pro/snowball/common/service/cmd/LoggerExtendService.java
+++ snowball-common/src/main/java/com/pro/snowball/common/service/cmd/LoggerExtendService.java
@@ -1,21 +1,25 @@
 package com.pro.snowball.common.service.cmd;
 
+import com.pro.common.modules.api.dependencies.message.ISocketSender;
 import com.pro.common.modules.api.dependencies.message.ToSocket;
 import com.pro.common.modules.service.dependencies.modelauth.base.MessageService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class LoggerExtendService {
-    MessageService messageService;
-
+//    MessageService messageService;
+    @Autowired
+    private ISocketSender socketSender;
     public void receiveLine(String orderKey, String line) {
         ToSocket toSocket = ToSocket.toAllUser("", line);
         toSocket.setTopic(orderKey);
-        messageService.sendToManager(toSocket);
+//        messageService.sendToManager(toSocket);
+        socketSender.send(toSocket);
         log.info(line);
     }
 }
